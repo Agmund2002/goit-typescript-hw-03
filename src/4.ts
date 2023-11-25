775a072e1dfa6a14671feda62a91c38426c2c2fb
Agmund2002 <malysh.andrey2000@gmail.com>
2023-11-25 20:02:43 +0200

    done all

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,42 @@
+class Key {
+  private signature: number = Math.random();
+
+  public getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  public getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door: boolean = false;
+  private tenants: Person[] = [];
+
+  constructor(public key: Key) {}
+
+  public comeIn(tenant: Person): void {
+    if (this.door) {
+      this.tenants.push(tenant);
+    }
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  public openDoor(key: Key): void {
+    if (key === this.key) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +45,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
